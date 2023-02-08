8370fe6075c4c5809242fd9d4d151427877264f1
priti73 <pritikumarinits@gmail.com>
2023-02-08 11:47:37 +0530

    task11

--- models/cart.js
+++ models/cart.js
@@ -33,4 +33,39 @@ module.exports= class Cart{
         });
     });
     }
+
+
+static deleteProduct(id,productPrice){
+fs.readFile(p,(err,fileContent)=>{
+    if(err){
+        return;
+    }
+    const updatedcart={...JSON.parse(fileContent)};
+    const product=updatedcart.products.find(prod=> prod.id===id);
+    if(!product){
+        return;
+    }
+    const productQty=product.qty;
+    updatedcart.products=updatedcart.products.filter(
+        prod=>prod.id!==id);
+    updatedcart.totalPrice=updatedcart.totalPrice-
+    productPrice*productQty;
+    fs.writeFile(p,JSON.stringify(updatedcart),err=>{
+        console.log(err);
+    });  
+})
+};
+
+static getCart(cb){
+    fs.readFile(p,(err,fileContent)=>{
+        const cart=JSON.parse(fileContent);
+        if(err){
+            cb(null);
+        } 
+        else{
+            cb(cart);
+        }
+});
+}
+
 }
