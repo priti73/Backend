ae4c12ec4824a173655fe5555d73b5ff6185f18e
priti73 <pritikumarinits@gmail.com>
2023-02-11 11:39:49 +0530

    expensetask

--- app.js
+++ app.js
@@ -34,7 +34,7 @@ sequelize
 });*/
 
 
-
+/*
 const path = require('path');
 
 const express = require('express');
@@ -43,11 +43,11 @@ const bodyParser = require('body-parser');
 const errorController = require('./controllers/error');
 const sequelize=require('./util/database');
 const User=require('./models/user');
-var cors =require('cors');
+//var cors =require('cors');
 
 const app = express();
 
-app.use(cors());
+//app.use(cors());
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
@@ -69,7 +69,47 @@ sequelize
 })
 .catch(err=>{
     console.log(err);
+});*/
+
+
+
+const path = require('path');
+
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const sequelize=require('./util/database');
+const User=require('./models/expense');
+var cors =require('cors');
+
+const app = express();
+
+app.use(cors());
+
+app.set('view engine', 'ejs');
+app.set('views', 'views');
+
+const expenseRoutes = require('./routes/expense');
+
+
+app.use(bodyParser.json({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+
+app.use(expenseRoutes);
+app.use(errorController.get404);
+
+sequelize
+.sync()
+.then(result=>{
+   app.listen(3000);
+})
+.catch(err=>{
+    console.log(err);
 });
+
+
 
 
 
