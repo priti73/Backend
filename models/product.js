e3e36e13c0a9719aa46e721cc0ac2fe0766ccd7b
priti73 <pritikumarinits@gmail.com>
2023-03-25 21:58:25 -0700

    update

--- models/product.js
+++ models/product.js
@@ -1,25 +1,56 @@
-const getDb=require('../util/database').getDb;
+const mongodb = require('mongodb');
+const getDb = require('../util/database').getDb;
 
-class Product{
-  constructor(title,price,imageUrl,description){
-    this.title=title,
-    this.imageUrl=imageUrl,
-    this.description=description,
-    this.price=price
+class Product {
+  constructor(title, price, description, imageUrl) {
+    this.title = title;
+    this.price = price;
+    this.description = description;
+    this.imageUrl = imageUrl;
   }
-  save(){
-   const db=getDb();
-   return db.collection('products')
-   .insertOne(this)
-   .then(result=>{
-    console.log(result)
-   })
-   .catch(err=>{
-    console.log(err)
-   });
+
+  save() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .insertOne(this)
+      .then(result => {
+        console.log(result);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static fetchAll() {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find()
+      .toArray()
+      .then(products => {
+        console.log(products);
+        return products;
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  }
+
+  static findById(prodId) {
+    const db = getDb();
+    return db
+      .collection('products')
+      .find({ _id: new mongodb.ObjectId(prodId) })
+      .next()
+      .then(product => {
+        console.log(product);
+        return product;
+      })
+      .catch(err => {
+        console.log(err);
+      });
   }
 }
 
-
-
-module.exports =Product;
+module.exports = Product;
